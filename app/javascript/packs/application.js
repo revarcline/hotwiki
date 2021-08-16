7e0aa97ba4975cda306d58ee1dd4df562f4ddfe1
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-16 16:27:45 -0500

    more tailwind tweaking, generated index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import "@hotwired/turbo-rails";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "../css/application.css";
 
-Rails.start()
-ActiveStorage.start()
+Rails.start();
+ActiveStorage.start();
